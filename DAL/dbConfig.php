56259e9da2282ff12b4ca61c1a3ec33d1e2320f6
jakobhostnik <jakob.hostnik@gmail.com>
2016-01-13 22:30:08 +0100

    asd

--- DAL/dbConfig.php
+++ DAL/dbConfig.php
@@ -1,14 +1,10 @@
 <?php
 class dbConfig{
-  //properties
   public static $server = "mysql.hostinger.co.uk";
   public static $user="u115822113_scode";
   public static $passwrod="slocode123";
   public static $database ="u115822113_forum";
-  //methods
   public static function connectDb(){
     return new mysqli($server, $user, $password, $databse);
   }
-}
-echo "2";
-?>
+}?>
